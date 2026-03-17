81e83abf8490a9bd3ce64e71cc6ac9f3e155f6fe
rherik <herikrosa@hotmail.com>
2026-03-17 16:24:19 -0300

    fix: remove llama_index e insere reauth para autenticação no basedosdados

--- src/dados_org.py
+++ src/dados_org.py
@@ -4,24 +4,52 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BILLING_PROJECT = os.environ['USER']
-
-query = """
-  SELECT
-    dados.ano as ano,
-    dados.sigla_uf AS sigla_uf,
-    diretorio_sigla_uf.nome AS sigla_uf_nome,
-    dados.tipo_eleicao as tipo_eleicao,
-    dados.data_eleicao as data_eleicao
-  FROM `basedosdados.br_tse_eleicoes.bens_candidato` AS dados
-  LEFT JOIN (SELECT DISTINCT sigla, nome FROM `basedosdados.br_bd_diretorios_brasil.uf`) AS diretorio_sigla_uf
-    ON dados.sigla_uf = diretorio_sigla_uf.sigla
-"""
-
-df = bd.read_sql(
-    query=query,
-    billing_project_id=BILLING_PROJECT
-    #from_file=True  # ESSENCIAL: força o uso das credenciais do arquivo definido em GOOGLE_APPLICATION_CREDENTIALS
-)
-
-print(df.head())
+class FonteBaseDosDados:
+  def __init__(self):
+    self.billing_id = os.environ['USER']
+    
+  def bens_deputado(self):
+    query_tudo_de_bens = """
+      SELECT
+        dados.ano as ano,
+        dados.sigla_uf AS sigla_uf,
+        diretorio_sigla_uf.nome AS sigla_uf_nome,
+        dados.id_eleicao as id_eleicao,
+        dados.tipo_eleicao as tipo_eleicao,
+        dados.data_eleicao as data_eleicao,
+        dados.titulo_eleitoral_candidato as titulo_eleitoral_candidato,
+        dados.sequencial_candidato as sequencial_candidato,
+        dados.tipo_item as tipo_item,
+        dados.descricao_item as descricao_item,
+        dados.valor_item as valor_item
+      FROM `basedosdados.br_tse_eleicoes.bens_candidato` AS dados
+      LEFT JOIN (SELECT DISTINCT sigla,nome  FROM `basedosdados.br_bd_diretorios_brasil.uf`) AS diretorio_sigla_uf
+          ON dados.sigla_uf = diretorio_sigla_uf.sigla
+      WHERE dados.ano = 2024 AND dados.sigla_uf = 'RJ'
+      LIMIT 10 
+    """
+    df = bd.read_sql(
+    query=query_tudo_de_bens,
+    billing_project_id=self.billing_id,
+    reauth=True
+    )
+    return df
+  
+  def frente_deputado(self):
+    query = """
+      SELECT
+        dados.id_frente as id_frente,
+        dados.id_deputado as id_deputado,
+        dados.titulo_deputado as titulo_deputado,
+        dados.nome_deputado as nome_deputado,
+        dados.titulo_deputado as titulo_deputado,
+        --dados.id_legislatura_deputado as id_legislatura_deputado
+      FROM `basedosdados.br_camara_dados_abertos.frente_deputado` AS dados
+      LIMIT 10
+    """
+    df = bd.read_sql(
+      query=query,
+      billing_project_id=self.billing_id,
+      reauth=True
+    )
+    return df
--- src/main.py
+++ src/main.py
@@ -2,18 +2,15 @@
 # from api_senado import APISenado
 import os
 from processador_dados import ProcessadorDadosCamara, ProcessadorDadosSenado
+from dados_org import FonteBaseDosDados
 import pandas as pd
 from api_camara import APICamara
 import logging
 from dotenv import load_dotenv
-from llama_index.core import Settings
-from llama_index.llms.groq import Groq
-from llama_index.experimental.query_engine.pandas import PandasQueryEngine
 
 load_dotenv()
-groq_key = os.environ['GROQ_KEY']
-api = APICamara()
-llm = Groq(model='llama-3.3-70b-versatile', api_key=groq_key)
+api_camara = APICamara()
+api_org = FonteBaseDosDados()
 
 # def servico_senado():
 #     api = APISenado()
@@ -25,29 +22,26 @@ llm = Groq(model='llama-3.3-70b-versatile', api_key=groq_key)
 #     print(f"Encontrados {len(df_senadores)} senadores")
 #     df_senadores.to_csv('../data/senadores.csv', index=False)
 
-def caminho_arquivo(params):
+def caminho_arquivo(params:str):
     base_dir = os.path.dirname(os.path.dirname(__file__))
     data_dir = os.path.join(base_dir, 'data')
     os.makedirs(data_dir, exist_ok=True)
-    csv_path = os.path.join(data_dir, f"deputados-{params}.csv")
+    csv_path = os.path.join(data_dir, f"deputados_{params}.csv")
     return csv_path
 
-def salva_arquivo(dataframe):
+def salva_arquivo(dataframe, params):
     """Salva DataFrame fornecido. Escrever nome do arquivo de acordo com os parâmetros"""
-    arquivo = dataframe.to_csv(caminho_arquivo(), index=False, sep=';')
+    arquivo = dataframe.to_csv(caminho_arquivo(params=params), index=False, sep=';')
     return arquivo
 
 def retorna_todos_deputados():
-    params = {'siglaPartido': 'PSOL', 'dataInicio': '2023-01-01', 'dataFim': '2023-12-31'}
-    deputados = api.busca_deputados_atual(**params)
+    params = {'dataInicio': '2024-01-01', 'dataFim': '2024-12-31'}
+    deputados = api_camara.busca_deputados_atual(**params)
     df_deputados = ProcessadorDadosCamara.deputados_para_dataframe(deputados['dados'])
     print(f"Encontrados {len(df_deputados)} deputados.")
-    # for index, deputado in df_deputados.iterrows():
-    #     print(f"{index+1} - {deputado['nome']} - {deputado['id']} - {deputado['siglaPartido']} - {deputado['idLegislatura']} - {deputado['siglaUf']}")
-    query_engine = PandasQueryEngine(df=df_deputados, llm=llm, verbose=True)
-    response = query_engine.query('Quais deputados tiveram mais de um mandato?')
-    print(response)
-
+    for index, deputado in df_deputados.iterrows():
+        print(f"{index+1} - {deputado['nome']} - {deputado['id']} - {deputado['siglaPartido']} - {deputado['idLegislatura']} - {deputado['siglaUf']}")
+    salva_arquivo(dataframe=df_deputados, params='2024')
     
 def detalhes_deputado(nome):
     usecols=["id", "nome"]
@@ -65,11 +59,20 @@ def detalhes_despesas(nome):
         if nome.lower() in row["nome"].lower():
             print(f"{row["nome"]} de ID {id_dep} encontrado(a) para exibir as despesas.\n")
 
-            despesas = api.busca_despesas(id=id_dep)
+            despesas = api_camara.busca_despesas(id=id_dep)
             df_despesas = ProcessadorDadosCamara.despesas_para_dataframe(despesas['dados'])
             print(df_despesas)
+            
+def fonte_base_dos_dados():
+    fonte_base_dos_dados = api_org.bens_deputado()
+    print(fonte_base_dos_dados)
+
+def fonte_frente_deputado():
+    fonte_base_dos_dados = api_org.frente_deputado()
+    print(fonte_base_dos_dados)
 
 if __name__ == "__main__":
     retorna_todos_deputados()
     #detalhes_despesas(nome='Talíria')
     #print(api.busca_deputados_atual())
+    #fonte_frente_deputado()
